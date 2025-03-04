9865bbae00f143579f8a55f2b110a56e8e9817b2
Kitten Films <kittenapps@icloud.com>
2025-03-04 18:58:05 +0500

    Update add.js

--- add.js
+++ add.js
@@ -1,1 +1,47 @@
+import { users } from "/GeoFS_Wiki/GeoFSUsers.js"
+import { get } from '/edit/code.js'
+import { Octokit } from "https://esm.sh/@octokit/core";
 
+/*var url_string = window.location.href; 
+var url = new URL(url_string);
+var user = url.searchParams.get("u");
+
+var url_string = window.location.href; 
+var url = new URL(url_string);
+var link = url.searchParams.get("l");*/
+
+export function update(link,user) {
+
+users[user] = link
+var content = `
+export var users = ${JSON.stringify(users)}
+`
+var url_string = window.location.href; 
+var url = new URL(url_string);
+var name = url.searchParams.get("n");
+
+const octokit = new Octokit({
+  auth: get(),
+})
+
+var old = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner: 'KittenApps-Films',
+  repo: 'GeoFS_Wiki',
+  path: 'GeoFSUsers.js',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+
+var newFile = await octokit.request('PUT /repos/{owner}/{repo}/contents/{path}', {
+  owner: 'KittenApps-Films',
+  repo: 'GeoFS_Wiki',
+  path: 'GeoFSUsers.js',
+  message: 'added to GeoFSUsers.js',
+  content: btoa(content),
+  sha: old.data.sha,
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  }
+})
+}
