0739648b854dc1705351f26d57e8292edfee5dc1
Kitten Films <kittenapps@icloud.com>
2025-03-06 12:59:44 +0500

    Create getter.js

---
+++ getter.js
@@ -0,0 +1,31 @@
+import { get } from '/edit/code.js'
+import { Octokit } from "https://esm.sh/@octokit/core";
+
+export async function getContents() {
+
+
+const octokit = new Octokit({
+  auth: get(),
+})
+
+var oldfile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+  owner: 'KittenApps-Films',
+  repo: 'GeoFS_Wiki',
+  path: 'GeoFSUsers.js',
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28',
+    'Accept' : 'application/vnd.github.html+json'
+  }
+})
+
+function getter(data) {
+  var a = atob(data)
+  var b = a.slice(19)
+  var c = JSON.parse(b)
+  return c
+}
+var filecontents = getter(oldfile.data.content)
+
+return filecontents
+}
+var b = update()
