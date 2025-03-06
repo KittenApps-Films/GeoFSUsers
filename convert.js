bd410e9ce45393aff0234749e1aae27578d4363d
Kitten Films <kittenapps@icloud.com>
2025-03-06 06:23:00 +0500

    Update convert.js

--- convert.js
+++ convert.js
@@ -1,9 +1,9 @@
 console.log("users installer running")
 import { users } from "/GeoFS_Wiki/GeoFSUsers.js"
-//import { update } from "./add.js"
+import { update } from "./add.js"
 import { one } from "./add.js"
 globalThis.users = users
-//globalThis.update = update
+globalThis.update = update
 console.log(one)
 console.log(one())
 globalThis.one = one
