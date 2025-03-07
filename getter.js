562b8791a20a29ebf369e05cfbbe4f96c861b3b1
Kitten Films <kittenapps@icloud.com>
2025-03-07 16:30:16 +0500

    Update getter.js

--- getter.js
+++ getter.js
@@ -1,4 +1,4 @@
-import { get } from '/edit/code.js'
+import { get } from 'https://kittenapps-films.github.io/edit/code.js'
 import { Octokit } from "https://esm.sh/@octokit/core";
 
 export async function getContents() {
@@ -14,22 +14,23 @@ var oldfile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}',
   path: 'GeoFSUsers.js',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28',
-    'Accept' : 'application/vnd.github.html+json'
+    'Accept' : 'application/vnd.github.raw+json'
   }
 })
-console.log(oldfile);
-console.log(oldfile.data.content);
+console.log(oldfile.data);
+//console.log(oldfile.data.content);
 function getter(data) {
-  console.log(data);
-  var a = atob(data)
-  console.log(a);
-  var b = a.slice(19)
+  //console.log(data);
+  //var a = dataatob(data)
+  //console.log(a);
+  var b = data.slice(19)//a.slice(19)
   console.log(b);
   var c = JSON.parse(b)
   console.log(c);
   return c
 }
-var filecontents = getter(oldfile.data.content)
+var filecontents = getter(oldfile.data)//.content)
 
 return filecontents
 }
+getContents()
