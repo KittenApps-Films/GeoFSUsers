dd8d71b44fdd7433a14098e1cf84371ab2070ba4
Kitten Films <kittenapps@icloud.com>
2025-03-07 17:01:54 +0500

    Update getter.js

--- getter.js
+++ getter.js
@@ -17,19 +17,14 @@ var oldfile = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}',
     'Accept' : 'application/vnd.github.raw+json'
   }
 })
-console.log(oldfile.data);
-//console.log(oldfile.data.content);
 function getter(data) {
-  //console.log(data);
-  //var a = dataatob(data)
-  //console.log(a);
-  var b = data.slice(19)//a.slice(19)
+  var b = data.slice(19)
   console.log(b);
   var c = JSON.parse(b)
   console.log(c);
   return c
 }
-var filecontents = getter(oldfile.data)//.content)
+var filecontents = getter(oldfile.data)
 
 return filecontents
 }
